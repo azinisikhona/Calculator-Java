1d22c00e543cad9cbb995d3b9c41973b2726e07b
Azinisikhona Siyalana <130047240+azinisikhona@users.noreply.github.com>
2024-05-20 10:53:29 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         
         System.out.println("Enter an integer: ");
